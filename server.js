c61ed70b09d4d0a6b333b051617a1518702e5c2a
benjarn374 <54038112+benjarn374@users.noreply.github.com>
2024-11-18 11:56:39 +0100

    client/Server

--- server.js
+++ server.js
@@ -10,18 +10,18 @@ ws.on('connection', socket => {
     // accueil du client et envoi de son id et du message de bienvenue
     const clientId = Date.now() + Math.random().toString(36).substr(2, 9);
     clients.push({ clientId, socket });
-    socket.send(JSON.stringify({ clientId: 'bot', yourId: clientId, text: "Bienvenue" }));
+    socket.send(JSON.stringify({ clientId: 'bot', pseudo: 'chatbot', yourId: clientId, text: "Bienvenue" }));
 
     // mécanique de traitement des messages du server
     socket.on('message', message => {
         console.log(`[WS] message: ${message}`);
         let messageObject = JSON.parse(message.toString());
 
-        if(messageObject.clientId && messageObject.pseudo){
+        if(messageObject.clientId && messageObject.newPseudo){
             clients.forEach(client => {
                 if (client.clientId === messageObject.clientId) {
-                    client.pseudo = messageObject.pseudo;
-                    client.socket.send(JSON.stringify({ clientId:'bot', text: 'Pseudo enregistré', pseudo: client.pseudo }));
+                    client.pseudo = messageObject.newPseudo;
+                    client.socket.send(JSON.stringify({ clientId:'bot', pseudo: 'chatbot', text: 'Pseudo enregistré' }));
                 }
             })
         }
@@ -35,8 +35,8 @@ ws.on('connection', socket => {
         }
 
         if (messageObject.text) {
-            ws.clients.forEach(client => {
-                client.send(JSON.stringify(messageObject));
+            clients.forEach(client => {
+                client.socket.send(JSON.stringify(messageObject));
             });
         }
     })
