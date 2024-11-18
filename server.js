50d1f65647dc1732d895e512f13b900255cc7560
benjarn374 <54038112+benjarn374@users.noreply.github.com>
2024-11-18 09:31:45 +0100

    server

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 // ce fichier sera notre server de WebSocket
 const Websocket = require('ws');
-const ws = new Websocket.Server({ port: 8080 });
+const ws = new Websocket.Server({ port: 8080 }); // Attention Xampp doit être fermé avant de lancer ce serveur
 console.log('[WS] Server started...');
