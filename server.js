1b4866873fb9603425cccc69a2f29990e1fe53d6
benjarn374 <54038112+benjarn374@users.noreply.github.com>
2024-11-18 11:23:42 +0100

    client/Server

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 // ce fichier sera notre server de WebSocket
 const Websocket = require('ws');
 const ws = new Websocket.Server({ port: 8080 }); // Attention Xampp doit être fermé avant de lancer ce serveur
+
+let clients = [];
+
 ws.on('connection', socket => {
     console.log('[WS] Un nouveau client est connecté.');
+
+    // accueil du client et envoi de son id et du message de bienvenue
+    const clientId = Date.now() + Math.random().toString(36).substr(2, 9);
+    clients.push({ id: clientId, socket });
+    socket.send(JSON.stringify({clientId, text: "Bienvenue"}));
+
+    // mécanique de traitement des messages du server
     socket.on('message', message => {
         console.log(`[WS] message: ${message}`);
         let messageObject = JSON.parse(message.toString());
         
         if(messageObject.text === '/time') {
             messageObject.text = new Date().toString();
+        }
+
+        if(messageObject.text === '/hello') {
+            messageObject.text = "Bonjour à tous";
         }
 
         ws.clients.forEach(client => {
